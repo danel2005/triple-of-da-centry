b777fb5f168f1f0a3d8fe737c809fe0c8f74e0f3
YonaPlayNoder <yonam2411@gmail.com>
2021-02-02 23:27:00 +0200

    shabotiz

---
+++ task1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+#define STRING_LENGHT 5
+
+int scanStr(char str[]);
+
+int main()
+{
+	char str[STRING_LENGHT] = {0};
+	int stringLenght = 0;
+	printf("Enter string (max lenght 100 chars): ");
+	stringLenght = scanStr(str);
+	printf("%d\n", stringLenght);
+	puts(str);
+	
+	return 0;
+}
+
+
+/*
+the function scan the string and delete the spaces
+input: string
+output: number
+*/
+int scanStr(char str[])
+{
+	int i = 0;
+	for(i = 0; i < STRING_LENGHT; i++)
+	{
+		scanf("%c", &str[i]);
+		
+		if(str[i] == '\n')
+		{
+			str[i] = 0;
+			return i;
+		}
+		
+		if(str[i] == ' ')
+		{
+			i = i - 1;
+		}
+	}
+	return i;
+}
+
