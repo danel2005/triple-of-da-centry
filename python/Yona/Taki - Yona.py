f8d1a828d2a958930a551c10fee067748b3441ba
YonaPlayNoder <yonam2411@gmail.com>
2021-02-02 20:52:37 +0200

    shabotiz

--- python/Yona/Taki - Yona.py
+++ python/Yona/Taki - Yona.py
@@ -1,1 +1,1 @@
-print("\"Shuffle, Shuffle, Shuffle\", say it together!\nChange colors and directions,\nDon't back down and stop the player!\t\tDo you want to play Taki?\t\tPress y\\n\"")
+print("\"Shuffle, Shuffle, Shuffle\", say it together!\nChange colors and directions,\nDon't back down and stop the player!\n\tDo you want to play Taki?\n\tPress y\\n")
