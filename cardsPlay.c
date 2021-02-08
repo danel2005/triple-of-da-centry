9aceca4d8aab6f57cdeb19735c652b757ad602b6
YonaPlayNoder <yonam2411@gmail.com>
2021-02-08 23:56:09 +0200

    hello

--- cardsPlay.c
+++ cardsPlay.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-#define	
+#define	SUM_CARDS 52
+#define SUN_SHAPES 4
+#define SHAPE_CARDS 13
 
 int main()
 {
+	int cards[SUN_SHAPES][SHAPE_CARDS] = {0}
+	int i = 0;
+	int j = 0;
 	
+	for(j = 0; J <= SUN_SHAPES; J++)
+	{
+		for(i = 0; i <= SHAPE_CARDS; ++i)
+		{
+			cards[j][i] = i;
+		}
+	}
 }
 
