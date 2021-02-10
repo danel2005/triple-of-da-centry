c85e7172d0f4a8d1a2cad3fef52354bc39b2b7c2
danel2005 <dcoolmineduck@gmail.com>
2021-02-10 19:09:39 +0200

    IDIOT

--- task4.c
+++ task4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define WORD_LEN 51
 #define SUM_WORDS 10
@@ -7,11 +8,11 @@
 int main(void)
 {
 	char words[SUM_WORDS][WORD_LEN] = {0};
-	int usedWord[26] = {0};
+	int usedWord[SUM_LETTERS] = {0};
 	int wordLen[SUM_WORDS] = {0};
 	int row = 0;
 	int i = 0;
-	int finish = 0;
+	int finish = 1;
 	int col = 0;
 	
 	printf("Enter up to 10 words, try to make a pangram: \n");
@@ -22,30 +23,27 @@ int main(void)
 		words[row][strcspn(words[row], "\n")] = 0;
 		wordLen[row] = strlen(words[row]);
 	
-		finish = 1;
-	
 		for(i = 0; i < wordLen[row]; i++)
 		{
-			usedWord[words[row][i] - 'a' + 1] = 1;
+			usedWord[words[row][i] - 'a'] = 1;
 		}
-		/*
-		for(i = 0; i < SUM_LETTERS; i++)
-		{
-			if(usedWord[i] == 0);
-			{
-				finish = 0;
-			}
-		}
-		*/
+
 	}
-	
-	finish = 1;
+
+	for(int i = 0; i < SUM_LETTERS; i++)
+	{
+		printf("%d ", usedWord[i]);
+		if(usedWord[i] == 0)
+		{
+			finish = 0;
+		}
+	}
+
 	if(finish == 1) // if pangram wroted
 	{
 		printf("It's a pangram?\nYes!");
 	}
-	
-	if(finish == 0) // if pangram wasnt wroted
+	else// if pangram not wroted
 	{
 		printf("It's a pangram?\nNo");
 	}
